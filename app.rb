7f1340ecb8cc28e77d4826a0b833d8cc4d6c5555
Matteo Restuccia <102557721+mrestuccia1@users.noreply.github.com>
2024-03-25 19:13:03 +0000

    done

--- app.rb
+++ app.rb
@@ -2,8 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:index)
+end
+
+get('/process_roll') do
+  @dice = params['dice'].to_i
+  @sides = params['sides'].to_i
+  @rolls = []
+
+  for i in (1..@dice)
+    roll = rand(1..@sides)
+    @rolls.push(roll)
+  end
+
+  erb(:rolls)
 end
